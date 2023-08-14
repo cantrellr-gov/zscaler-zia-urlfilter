f88ddc6c68fe5eccc264456e2a5977c99f38ef26
cantrellr-gov <charles.cantell@usdoj.gov>
2023-08-14 12:16:44 -0400

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "efa_urlfilter_id" {
     value = zia_url_filtering_rules.efa_urlfilter.id
 }
 output "efa_urlfilter_name" {
-    value = zia_url_filtering_rules.efa_urlfilter.efa_urlfilter_name
+    value = zia_url_filtering_rules.efa_urlfilter.name
 }
